73edd9e0ab25a2b1d54275daf202e98cdd72b604
Nicklas Ahlskog <kivi@kivibot.fi>
2016-08-18 01:54:06 +0300

    Bug simulation

---
+++ BugSim/Bug/Bug.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugSim.Bug
+{
+    public class Bug
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Rotation { get; set; }
+
+        public double ForwardPower { get; set; }
+        public double AngularPower { get; set; }
+    }
+}
